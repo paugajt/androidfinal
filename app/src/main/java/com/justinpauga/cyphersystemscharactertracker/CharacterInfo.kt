55a7e484901d1af182a83507608672816259486b
Pauga <justin.pauga@C02NLBK7G3QD.servicemagic.com>
2018-04-15 10:59:46 -0600

    Added .kt classes for all layouts

--- app/src/main/java/com/justinpauga/cyphersystemscharactertracker/CharacterInfo.kt
+++ app/src/main/java/com/justinpauga/cyphersystemscharactertracker/CharacterInfo.kt
@@ -1,12 +1,12 @@
 package com.justinpauga.cyphersystemscharactertracker
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class CharacterInfo() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.character_info)
     }
 }
