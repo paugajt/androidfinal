793b935f42359a34d85d51d662ed0c39df6ec149
Sang Nguyen <31318336+snguye39@users.noreply.github.com>
2018-04-15 19:27:10 -0600

    last

--- app/src/main/java/com/justinpauga/cyphersystemscharactertracker/NewRoundActivity.kt
+++ app/src/main/java/com/justinpauga/cyphersystemscharactertracker/NewRoundActivity.kt
@@ -1,5 +1,14 @@
 package com.justinpauga.cyphersystemscharactertracker
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
 /**
  * Created by sangnguyen on 4/13/18.
  */
+class NewRoundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_round)
+    }
+}
