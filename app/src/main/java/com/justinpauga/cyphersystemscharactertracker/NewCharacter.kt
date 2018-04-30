275fecfa85645a1b9be898c9177f44cdd7535e53
Justin Pauga <justin.pauga@Justins-MacBook-Pro.local>
2018-04-30 14:24:23 -0600

    Added in checks for new character information

--- app/src/main/java/com/justinpauga/cyphersystemscharactertracker/NewCharacter.kt
+++ app/src/main/java/com/justinpauga/cyphersystemscharactertracker/NewCharacter.kt
@@ -25,16 +25,87 @@ class NewCharacter() : AppCompatActivity() {
     fun saveNewCharacter(view: View) {
 
             var character = Character::class.java.newInstance()
-            character.setName(create_name.text.toString())
-            character.setDescriptor(create_descriptor.text.toString())
-            character.setType(create_type.text.toString())
-            character.setFocus(create_focus.text.toString())
-            character.setTier(create_tier.text.toString().toInt())
-            character.setEffort(create_effort.text.toString().toInt())
-            character.setXp(create_xp.text.toString().toInt())
-            character.setMight(create_might.text.toString().toInt())
-            character.setSpeed(create_speed.text.toString().toInt())
-            character.setIntelligence(create_intellect.text.toString().toInt())
+            if(create_name.text.toString() == "") {
+                val toast = Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setName(create_name.text.toString())
+            }
+            if(create_descriptor.text.toString() == "") {
+                val toast = Toast.makeText(this, "Please enter a descriptor", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setDescriptor(create_descriptor.text.toString())
+            }
+            if(create_type.text.toString() == "") {
+                val toast = Toast.makeText(this, "Please enter a type", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setType(create_type.text.toString())
+            }
+            if(create_focus.text.toString() == "") {
+                val toast = Toast.makeText(this, "Please enter a focus", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setFocus(create_focus.text.toString())
+            }
+            if(create_tier.text.toString().toInt() < 1) {
+                val toast = Toast.makeText(this, "Please enter a valid tier", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setTier(create_tier.text.toString().toInt())
+            }
+            if(create_effort.text.toString().toInt() < 0) {
+                val toast = Toast.makeText(this, "Please enter a valid effort", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setEffort(create_effort.text.toString().toInt())
+            }
+            if(create_xp.text.toString().toInt() < 0) {
+                val toast = Toast.makeText(this, "Please enter a valid number for xp", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setXp(create_xp.text.toString().toInt())
+            }
+            if(create_might.text.toString().toInt() < 1) {
+                val toast = Toast.makeText(this, "Please enter a valid might value", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setMight(create_might.text.toString().toInt())
+            }
+            if(create_speed.text.toString().toInt() < 1) {
+                val toast = Toast.makeText(this, "Please enter a valid speed value", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setSpeed(create_speed.text.toString().toInt())
+            }
+            if(create_intellect.toString().toInt() < 1) {
+                val toast = Toast.makeText(this, "Please enter a valid intellect value", Toast.LENGTH_LONG)
+                toast.show()
+                return
+            }
+            else {
+                character.setIntelligence(create_intellect.text.toString().toInt())
+            }
+
             character.setAbilities(create_abilities.text.toString())
             character.setAttacks(create_attacks.text.toString())
             character.setCyphers(create_cyphers.text.toString())
