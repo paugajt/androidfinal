9dcaceba40f6b82106ff5dc0fbdbf042aebad174
Sang Nguyen <31318336+snguye39@users.noreply.github.com>
2018-04-30 16:42:02 -0600

    edit char

--- app/src/main/java/com/justinpauga/cyphersystemscharactertracker/EditCharacterInfo.kt
+++ app/src/main/java/com/justinpauga/cyphersystemscharactertracker/EditCharacterInfo.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.edit_character.*
+import java.io.File
 
 class EditCharacterInfo() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +18,10 @@ class EditCharacterInfo() : AppCompatActivity() {
     }
 
     fun cancelEditCharacter(view: View){
-        val cancelButton = Intent(this, CharacterInfo::class.java)
-        startActivity(cancelButton)
+//        val cancelButton = Intent(this, CharacterInfo::class.java)
+//        startActivity(cancelButton)
+        setResult(Activity.RESULT_CANCELED)
+        finish()
     }
     fun editButtonIntent(){
         val info = intent.extras
@@ -40,12 +44,51 @@ class EditCharacterInfo() : AppCompatActivity() {
             edit_notes.setText(char.getNotes(), TextView.BufferType.EDITABLE)
         }
     }
+    fun saveButtonIntent(view: View){
+        var character = Character::class.java.newInstance()
+        character.setName(edit_name.text.toString())
+        character.setDescriptor(edit_descriptor.text.toString())
+        character.setType(edit_type.text.toString())
+        character.setFocus(edit_focus.text.toString())
+        character.setTier(edit_tier.text.toString().toInt())
+        character.setEffort(edit_effort.text.toString().toInt())
+        character.setXp(edit_xp.text.toString().toInt())
+        character.setMight(edit_might.text.toString().toInt())
+        character.setSpeed(edit_speed.text.toString().toInt())
+        character.setIntelligence(edit_intellect.text.toString().toInt())
+        character.setAbilities(edit_abilities.text.toString())
+        character.setAttacks(edit_attacks.text.toString())
+        character.setCyphers(edit_cyphers.text.toString())
+        character.setEquipment(edit_equipment.text.toString())
+        character.setNotes(edit_notes.text.toString())
+        sendCharacter(view,character)
+    }
+    fun deleteChar(){
+        val info = intent.extras
+        val character = info.getSerializable("character") as Character
+        val file = File(filesDir, character.getName())
+        file.delete()
+    }
+    fun saveChar(){
+        val info = intent.extras
+        val character = info.getSerializable("character") as Character
+        val file = File(filesDir, character.getName())
+        if (!file.exists()) {
+            val out = file.printWriter()
+            out.print("${character.getName()},${character.getDescriptor()},${character.getType()}," +
+                    "${character.getFocus()},${character.getTier()},${character.getEffort()}," +
+                    "${character.getXp()}, ${character.getMight()}, ${character.getSpeed()}," +
+                    "${character.getIntelligence()},${character.getAbilities()}," +
+                    "${character.getAttacks()},${character.getCyphers()},${character.getEquipment()}," +
+                    "${character.getNotes()}")
+            out.close()
+        }
+    }
+    fun sendCharacter(view: View, character: Character) {
+        var intent = Intent()
+        intent.putExtra("EditChar", character)
+        finish()
+    }
 
-//    fun sendCharacter(view: View, character: Character) {
-//        var intent = Intent()
-//        intent.putExtra("Character", character)
-//        setResult(Activity.RESULT_OK, intent)
-//        finish()
-//    }
 
 }
