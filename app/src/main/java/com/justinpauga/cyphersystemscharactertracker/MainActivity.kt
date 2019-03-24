2c95297897865cfd05f1060b042c7c513b9b027c
Justin Pauga <>
2019-03-24 15:30:06 -0600

    Fixed stuff

--- app/src/main/java/com/justinpauga/cyphersystemscharactertracker/MainActivity.kt
+++ app/src/main/java/com/justinpauga/cyphersystemscharactertracker/MainActivity.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import java.io.File
 import android.app.AlertDialog
 import android.view.Gravity
